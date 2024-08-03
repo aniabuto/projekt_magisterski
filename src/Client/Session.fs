3d7f173d56f49bf907fb28c88bd32a598f16fd88
aniabuto <aniabuto@wp.pl>
2024-08-03 18:18:07 +0200

    add cart session settings

--- src/Client/Session.fs
+++ src/Client/Session.fs
@@ -5,6 +5,7 @@ open Thoth.Json
 
 [<Literal>]
 let USER_SESSION_KEY = "user"
+let CART_SESSION_KEY = "cart"
 
 let loadUser () : UserData option =
     let userDecoder = Decode.Auto.generateDecoder<UserData> ()
@@ -16,3 +17,14 @@ let loadUser () : UserData option =
 let deleteUser () = LocalStorage.delete USER_SESSION_KEY
 
 let saveUser (user: UserData) = LocalStorage.save USER_SESSION_KEY user
+
+let loadCart () : CartDetails option =
+    let cartDecoder = Decode.Auto.generateDecoder<CartDetails> ()
+
+    match LocalStorage.load cartDecoder CART_SESSION_KEY with
+    | Ok cart -> Some cart
+    | Error _ -> None
+
+let saveCart (cart : CartDetails) = LocalStorage.save CART_SESSION_KEY cart
+
+let deleteCart () = LocalStorage.delete CART_SESSION_KEY
